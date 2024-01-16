518fde152b236af086060994bf450ab2a798ff7e
rov1dev <nazarov1.off@gmail.com>
2024-01-16 09:43:35 +0500

    main

---
+++ 6randomPassword/script.js
@@ -0,0 +1,26 @@
+const passwordInput = document.querySelector('.password-box input'),
+    cupyIcon = document.querySelector('.password-box .copy-icon'),
+    rangeInput = document.querySelector('.range-box input'),
+    sliderNumber = document.querySelector('.range-box .slider-number'),
+    generateButton = document.querySelector('.generate-button');
+console.log(cupyIcon);
+const allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ1234567890";
+const generatePassword = () => {
+    let newPassword = ""
+
+    for (let i = 0; i < rangeInput.value; i++) {
+        let randomNubers = Math.floor(Math.random() * allCharacters.length)
+        newPassword += allCharacters[randomNubers]
+    }
+    passwordInput.value = newPassword
+    cupyIcon.classList.replace("uil-file-check-alt", "uil-copy")
+}
+rangeInput.addEventListener("input", () => {
+    sliderNumber.innerText = rangeInput.value;
+    generatePassword();
+})
+cupyIcon.addEventListener("click", () => {
+    navigator.clipboard.writeText(passwordInput.value)
+    cupyIcon.classList.replace("uil-copy","uil-file-check-alt")
+})
+generateButton.addEventListener("click", generatePassword)
---
+++ 8otpCode/script.js
@@ -0,0 +1,4 @@
+const inputs = document.querySelectorAll("input"),
+button = document.querySelector("button");
+
+window.addEventListener("load",()=> inputs[0].focus())
