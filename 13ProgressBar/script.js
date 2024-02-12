9d6b23ffba9f7765af973447d82bfdc12a13c8bf
rov1dev <rahmatillo722022@gmail.com>
2024-02-12 10:42:33 +0500

    main

---
+++ 13ProgressBar/script.js
@@ -0,0 +1,16 @@
+let circularProgress = document.querySelector(".circularProgress"),
+    progressValue = document.querySelector(".progressValue");
+
+let progressStartValue = 0,
+    progressEndValue = 90,
+    speed = 100;
+let progress = setInterval(() => {
+    progressStartValue++
+
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(#7d2ae8 
+        ${progressStartValue *3.6}deg, #ededed 0deg)`
+    if (progressStartValue == progressEndValue) {
+        clearInterval(progress)
+    }
+}, speed);
