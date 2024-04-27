35cbfb6179fee07b191877c4cf490cf99148c0a0
rov1dev <rahmatillo722022@gmail.com>
2024-04-27 15:41:38 +0500

    main

--- 20ImageZoom/script.js
+++ 20ImageZoom/script.js
@@ -1,51 +1,21 @@
-let imageContainer = document.querySelector(".imageContainer")
-let image = document.querySelector(".image")
-let overlay = document.querySelector(".overlay")
-let mouse = document.querySelector(".mouse")
+    const imageContainer = document.querySelector(".image-container");
+    const zoomOverlay = document.querySelector(".zoom-overlay");
 
-let events = {
-    mouse: {
-        move:"mousemove"
-    },
-    touch:{
-        move:"touchmove"
+    imageContainer.addEventListener("mousemove", function (event) {
+        const { offsetX, offsetY } = event;
+        const { offsetWidth, offsetHeight } = this;
+
+        const xPos = (offsetX / offsetWidth) * 100;
+        const yPos = (offsetY / offsetHeight) * 100;
+
+        zoomOverlay.style.backgroundPosition = `${xPos}% ${yPos}%`;
+    });
+
+    imageContainer.addEventListener("mouseenter", function () {
+        zoomOverlay.classList.add("active");
+    });
+
+    imageContainer.addEventListener("mouseleave", function () {
+        zoomOverlay.classList.remove("active");
     }
-}
-let device = ""
-
-function Device() {
-    try {
-        device: "touch"
-        document.createElement("TouchEvent")
-        return true
-    }
-     catch(e) {
-        device = "mouse"
-        return false
-     } 
-}
-const hideElement = ()=>{
-    overlay.style.display = "none"
-    mouse.style.display = "none"
-}
-Device()
-
-imageContainer.addEventListener(events[device].move, (e)=>{
-    try {
-        let x = !Device ? e.pageX : e.touches[0].pageX
-        let y = !Device ? e.pageY : e.touches[0].pageY
-    }
-     catch(e) {
-        let imageWidth =  imageContainer.offsetWidth
-        let imageHeight = imageContainer.offsetHeight
-
-        if (imageWidth - (x-imageContainer.offsetWidth)< 15 || x-imageContainer.offSetLeft < 15 ||
-        imageHeight - (y-imageContainer.offSetTop) < 15 || y-imageContainer.offSetTop < 15) {
-            hideElement()
-        }
-         else {
-            overlay.style.display = "block"
-            mouse.style.display = "inline-block"
-         }
-     }
-})
+    )
