82b8de0351ee60d85e2f95dbf572215cad608bb4
Umarov Rahmatillo <rahmatillo722022@gmail.com>
2024-07-19 18:25:11 +0500

    main

--- 4sliderjs/script.js
+++ 4sliderjs/script.js
@@ -1,17 +1,18 @@
-let wrapper = document.querySelector(".wrapper")
 let carousel = document.querySelector(".carousel")
 let images = document.querySelectorAll("img")
 let buttons = document.querySelectorAll(".button");
 
     let imageIndex = 1
-    let intervalId = ""
+    let intervalId = "";    
+  
     const slideImage = () => {
         imageIndex = imageIndex === images.length ? 0 : imageIndex < 0 ? images.length - 1 : imageIndex
-        carousel.transfor = `translate(${imageIndex * 100}%)`
+        carousel.style.transform = `translateX(-${imageIndex * 100}%)`
 }
 const updateClick = (e) => {
     clearInterval(intervalId)
-    imageIndex = e.target.id === "next" ? 1 : -1;
+    imageIndex += e.target.id === "next" ? 1 : -1;
     slideImage(imageIndex)
 }
-button.forEach(button => button.addEventListener("", updateClick))
+
+buttons.forEach(button => button.addEventListener("click", updateClick))
